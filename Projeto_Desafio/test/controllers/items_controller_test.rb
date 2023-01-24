460d4a94e4e9a5f100981744893482024ae36079
Gabriel R. Ribeiro <gabrielwork01@gmail.com>
2023-01-24 11:47:51 -0300

    Correção logout e criação de novos testes

--- Projeto_Desafio/test/controllers/items_controller_test.rb
+++ Projeto_Desafio/test/controllers/items_controller_test.rb
@@ -5,25 +5,19 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
     @item = items(:completo)
   end
  
-  test "Deve mostrar o item" do
+  test 'Deve mostrar o item' do
     get item_url(@item)
     assert(@item)
   end
 
-  # test "Deve editar o item" do
-  #   patch item_url(@item), params: { item: { arquivo: @item.arquivo, item_description: @item.item_description, item_price: @item.item_price, merchant_address: @item.merchant_address, merchant_name: @item.merchant_name, purchase_count: @item.purchase_count, purchaser_name: @item.purchaser_name } }
-  # end
-
-  test "Deve excluir o item" do
+  test 'Deve excluir o item' do
     assert_difference("Item.count", 0) do
       delete item_url(@item)
     end
   end
 
-  test "ID do usuário não pode ser nulo" do
+  test 'ID do usuário não pode ser nulo' do
     assert_not_nil @item.user_id
   end  
-
-  
 
 end
